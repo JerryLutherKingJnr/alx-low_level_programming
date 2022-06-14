e86eef9e77ffe07e77380072d760b6fcb076f385
Onyeka Egbune <egbuneonyeka@gmail.com>
2022-06-13 17:38:15 -0700

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
-void void reset_to_98(int *n);
+void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
